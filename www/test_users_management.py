a703f0971521e0da3256571c0fc281af46e9cc16
Gerold103 <v.shpilevoy@tarantool.org>
2017-02-22 04:01:54 +0300

    Implement tests on users management
    Closes #39

--- www/test_users_management.py
+++ www/test_users_management.py
@@ -244,3 +244,247 @@ class TestSuiteUsersManagement(BaseTestSuite):
 			#
 			self.assertIn('user_was_created', response)
 		logger.info('ok - permit normal users')
+
+	@tornado.testing.gen_test
+	def test3_test_users_show(self):
+		application.connect_db()
+		#
+		# First, we test the users management page under
+		# the admin account.
+		#
+		logged = yield self.get_logged()
+		client = self.get_client()
+		headers = logged['headers']
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='GET')
+		body = response.body.decode('utf-8')
+		#
+		# The user with right on users edit must see the
+		# following labels on the page.
+		#
+		self.assertIn('Управление пользователями', body)
+		self.assertNotIn('user_was_created', body)
+		self.assertNotIn('user_was_deleted', body)
+		self.assertNotIn('user_was_edited', body)
+		self.assertIn('Добавить пользователя', body)
+		self.assertIn('modal fade user-modal-window', body)
+		self.assertIn('Вперед <span aria-hidden="true">&rarr;</span>',
+			      body)
+		self.assertNotIn('<span aria-hidden="true">&larr;</span> Назад',
+				 body)
+		logger.info('ok - header of users page, if the current user '\
+			    'can edit')
+		# Check that on the page the correct list of users
+		# is showed.
+		#
+		all_inserted = list(test_normal_users)
+		all_inserted.extend(test_users)
+		self.assertEqual(len(all_inserted), 12)
+		key = lambda x: (not x['name'], x['name'], x['email'])
+		all_inserted = sorted(all_inserted, key=key)
+		for i, user in enumerate(all_inserted):
+			if i >= USERS_ON_PAGE:
+				break;
+			self.assertIn('<td>%s</td>' % user['email'], body)
+		logger.info('ok - first page of users list')
+		#
+		# Try to get the second page.
+		#
+		url = '%s?page=2' % self.get_url('/users_management')
+		response = yield client.fetch(url, headers=headers,
+					      method='GET')
+		body = response.body.decode('utf-8')
+		self.assertNotIn('Вперед <span aria-hidden="true">&rarr;</span>',
+				 body)
+		self.assertIn('<span aria-hidden="true">&larr;</span> Назад',
+			      body)
+		for i, user in enumerate(all_inserted):
+			if i < USERS_ON_PAGE:
+				continue
+			if i >= USERS_ON_PAGE * 2:
+				break;
+			self.assertIn('<td>%s</td>' % user['email'], body)
+		logger.info('ok - second page of users list')
+		#
+		# Get incorrect page.
+		#
+		url = self.get_url('/users_management')
+		urls = [ '%s?page=-1' % url, '%s?page=abc' % url,
+			 '%s?page=1000' % url ]
+		for url in urls:
+			response = yield client.fetch(url, headers=headers,
+						      method='GET')
+			body = response.body.decode('utf-8')
+			self.assertIn(ERR_PAGE_NUMBER, body)
+		logger.info('ok - incorrect page numbers')
+		#
+		# Make incorrect action.
+		#
+		url = self.get_url('/users_management')
+		urls = [ '%s?action=del' % url, '%s?action=create' % url,
+			 '%s?action=edit' % url, '%s?action=^' % url,
+			 '%s?action=123' % url ]
+		for url in urls:
+			response = yield client.fetch(url, headers=headers,
+						      method='GET')
+			body = response.body.decode('utf-8')
+			self.assertIn(ERR_ACTION, body)
+		logger.info('ok - incorrect actions')
+
+	@tornado.testing.gen_test
+	def test4_delete_user(self):
+		application.connect_db()
+		logged = yield self.get_logged()
+		client = self.get_client()
+		headers = logged['headers']
+		user = test_normal_users[0]
+		tx = yield application.begin()
+		id = yield get_user_by_email(tx, ['id', ], user['email'])
+		id = id['id']
+		url = '{}?id={}&action=delete'\
+		      .format(self.get_url('/users_management'), id)
+		response = yield client.fetch(url, headers=headers,
+					      method='GET')
+		body = response.body.decode('utf-8')
+		self.assertIn('user_was_deleted', body)
+		self.assertNotIn(user['email'], body)
+		yield tx.commit()
+		logger.info('ok - user was deleted')
+		#
+		# Recreate the deleted user.
+		#
+		yield self.post_user(user)
+
+	@tornado.testing.gen_test
+	def test5_edit_user(self):
+		application.connect_db()
+		logged = yield self.get_logged()
+		client = self.get_client()
+		headers = logged['headers']
+		user = test_normal_users[0]
+		self.assertEqual(user['name'], 'John Doe 1')
+		self.assertEqual(user['email'], 'email1@mail.ru')
+		post = dict(user)
+		#
+		# Edit email
+		#
+		tx = yield application.begin()
+		id = yield get_user_by_email(tx, ['id', ], user['email'])
+		yield tx.commit()
+		id = id['id']
+		post['email'] = 'email1_new@mail.ru'
+		post['action'] = 'edit'
+		post['id'] = id
+		post['password'] = ''
+		post['password-repeat'] = ''
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertIn('user_was_edited', body)
+		self.assertIn(post['email'], body)
+		logger.info("ok - user's email is edited")
+		user['email'] = post['email']
+		#
+		# Change email to the existing email. Also we try
+		# to change the password, but is must not happen,
+		# since the duplicate error occures.
+		#
+		self.assertGreaterEqual(len(test_normal_users), 1)
+		another_user = test_normal_users[1]
+		post['email'] = another_user['email']
+		post['password'] = 'new_password'
+		post['password-repeat'] = 'new_password'
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertNotIn('user_was_edited', body)
+		self.assertIn(ERR_INSERT, body)
+		#
+		# The user still can login with the old password.
+		#
+		response = yield self.login_user(user['email'],
+						 user['password'])
+		self.assertNotIn('error', response)
+		self.assertIn('headers', response)
+		logger.info("ok - it is forbidden to choose the existing email"\
+			    " during an user editing")
+		#
+		# Send the old password as edited.
+		#
+		post = dict(user)
+		post['action'] = 'edit'
+		post['id'] = id
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertIn('user_was_edited', body)
+		self.assertIn(post['email'], body)
+		#
+		# The user still can login.
+		#
+		response = yield self.login_user(user['email'],
+						 user['password'])
+		self.assertNotIn('error', response)
+		self.assertIn('headers', response)
+		logger.info('ok - an user can keep the old password')
+		#
+		# Try to set incorrect passwords.
+		#
+		post['password'] = '' # password-repeat remains.
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertNotIn('user_was_edited', body)
+		self.assertIn(ERR_PASSWORD_ABSENSE, body)
+
+		post['password'] = '123' # too short password.
+		post['password-repeat'] = '123'
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertNotIn('user_was_edited', body)
+		self.assertIn(ERR_PASSWORD_LENGTH, body)
+
+		# Password differs from password-repeat.
+		post['password'] = '123456'
+		post['password-repeat'] = '1234567'
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertNotIn('user_was_edited', body)
+		self.assertIn(ERR_PASSWORD_MATCH, body)
+		logger.info('ok - new password can not be invalid')
+		#
+		# Edit the current user.
+		#
+		post = {}
+		post['email'] = logged['email']
+		post['action'] = 'edit'
+		post['id'] = logged['id']
+		post['see_reports'] = 'on'
+		post['upload_reports'] = 'on'
+		post['delete_reports'] = 'on'
+		post['see_users'] = 'on'
+		post['edit_users'] = 'on'
+		self.assertNotEqual('Great Admin', logged['name'])
+		post['name'] = 'Great Admin'
+		body = urllib.parse.urlencode(post)
+		response = yield client.fetch(self.get_url('/users_management'),
+					      headers=headers, method='POST',
+					      body=body)
+		body = response.body.decode('utf-8')
+		self.assertIn('user_was_edited', body)
+		self.assertIn('Вы вошли как <b>%s</b>' % post['name'], body)
+		logger.info('ok - edit the current user')
